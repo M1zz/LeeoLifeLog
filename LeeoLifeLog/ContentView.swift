26d8495b5482f28f4cbe29c075bf2065ba5d8be1
hyunho <mizzking75@gmail.com>
2022-06-19 22:08:21 +0900

    Initial Commit

---
+++ LeeoLifeLog/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LeeoLifeLog
+//
+//  Created by hyunho lee on 2022/06/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LeeoLifeLog/LeeoLifeLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeeoLifeLogApp.swift
+//  LeeoLifeLog
+//
+//  Created by hyunho lee on 2022/06/19.
+//
+
+import SwiftUI
+
+@main
+struct LeeoLifeLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
